8f9530d61834daa764b9f2e1454805735a9b2821
kilo <39413400+kil0byt3@users.noreply.github.com>
2023-06-16 12:10:07 -0700

    Update Searchbar.tsx

--- src/Startpage/Searchbar/Searchbar.tsx
+++ src/Startpage/Searchbar/Searchbar.tsx
@@ -78,7 +78,7 @@ export const Searchbar = () => {
     <StyledSearchbarContainer>
       {searchSymbol && <SearchIcon src={searchSymbol} />}
       <StyledSearchbar
-        placeholder="Always stay clean!"
+        placeholder="hello kilo"
         type="input"
         onKeyUp={e => e.which === 13 && redirectToSearch(e.currentTarget.value)}
         autoFocus
